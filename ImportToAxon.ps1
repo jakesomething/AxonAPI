2dc827d6d119b58ce29acb572014113f52319c94
jakesomething <jakesomething@hotmail.com>
2022-12-29 16:16:47 -0700

    Added comments

--- ImportToAxon.ps1
+++ ImportToAxon.ps1
@@ -1,22 +1,31 @@
+#
+# Powershell script to push indicators from CSV into LogRhythm Axon lists. This is configured to append IPs to a list.
+# Written by github.com/jakesomething 12-29-2022
+#
+
 $api_key = ""
 $tenant_id = ""
 $list_id = ""
+$FilePath = '/Exports/IPV4s.csv'
 
+#Configure headers
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Content-Type", "multipart/form-data")
 $headers.Add("Authorization", $api_key)
 
+#Setup file to be uplaoded
 $multipartContent = [System.Net.Http.MultipartFormDataContent]::new()
-$multipartFile = '/C:/Exports/IPV4s.csv'
+$multipartFile = $FilePath
 $FileStream = [System.IO.FileStream]::new($multipartFile, [System.IO.FileMode]::Open)
 $fileHeader = [System.Net.Http.Headers.ContentDispositionHeaderValue]::new("form-data")
 $fileHeader.Name = "file"
-$fileHeader.FileName = "/C:/Exports/IPV4s.csv"
+$fileHeader.FileName = $FilePath
 $fileContent = [System.Net.Http.StreamContent]::new($FileStream)
 $fileContent.Headers.ContentDisposition = $fileHeader
 $multipartContent.Add($fileContent)
 
 $body = $multipartContent
 
-$response = Invoke-RestMethod 'https://api.na01.prod.boreas.cloud/list-svc/v1/tenants/' + $tenant_id + '/list-definitions/' + $list_id + '/list-items/import?headersNotIncluded=false' -Method 'POST' -Headers $headers -Body $body
+#Send CSV to Axon
+$response = Invoke-RestMethod "https://api.na01.prod.boreas.cloud/list-svc/v1/tenants/$tenant_id/list-definitions/$list_id/list-items/import?headersNotIncluded=false" -Method 'PUT' -Headers $headers -Body $body
 $response | ConvertTo-Json
